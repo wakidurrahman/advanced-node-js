22c7117b2d04e8a3e2b97c03358d152c1c206b9f
wakidurrahman <wakidur.rahman@4digit.jp>
2024-01-04 19:13:09 +0900

    Implementing Sub-Commands 2

--- docs/books/introduction-to-nodejs/building-cli-tools/cli-tools/src/utils.js
+++ docs/books/introduction-to-nodejs/building-cli-tools/cli-tools/src/utils.js
@@ -9,12 +9,22 @@ const API = "http://localhost:3000";
 // Set the categories
 const categories = ["confectionery", "electronics"];
 
+// Log the usage of the command to the console
+export const log = (message) => {
+  console.log(`\n${message}\n`);
+};
+
+// Log the error to the console
+export const error = (message) => {
+  console.error(`\n${message}\n`);
+};
+
 // Update the order with the given ID
 export async function updateCategory(id, amount) {
   console.log(`Updating order ${id} with amount ${amount}`);
   try {
     if (isNaN(+amount)) {
-      console.log("Error: <AMOUNT> must be a number");
+      log("Error: <AMOUNT> must be a number");
       process.exit(1);
     }
     // Use `got` HTTP request library for Node.js to make a POST request to the API.
@@ -33,6 +43,46 @@ export async function updateCategory(id, amount) {
      * and then exit the process with a code of 1 to indicate it was not successful.
      */
     console.error(error.message);
+    process.exit(1);
+  }
+}
+
+// Add a new Order
+
+/**
+ * Add a new Order
+ * 
+ * we are passing through all the arguments ...args that would be provided to the function.
+ * This allows us to destructure and extract the values that we need, 
+ * to allow us to provide the additional functionality that we require. 
+ * @param  {...any} args 
+ */
+
+export async function addCategory(...args) {
+  // Destructure the arguments
+  let [category, id, name, amount, info] = args;
+  log(`Adding item ${id} with amount ${amount}`);
+
+  try {
+    // we force the type of the amount to be an integer by using the '+' before the amount 
+    if (isNaN(+amount)) {
+      error(`Error: <AMOUNT> must be a number`);
+      process.exit(1);
+    }
+    // use GOT library to make a POST request to the API
+    await got.post(`${API}/${category}`, {
+      json: {
+        id,
+        name,
+        rrp: +amount,
+        info: info.json(" "),
+      },
+    });
+    // Log the result to the console
+    log(`Item "${id}: ${name}" has been added to the ${category} category`);
+  } catch (error) {
+    // If there is an error, log it to the console and exit
+    error(error.message);
     process.exit(1);
   }
 }
