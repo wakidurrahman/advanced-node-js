3aa872341f3728e1237e5a7406b29855ee237645
wakidurrahman <wakidur.rahman@4digit.jp>
2024-01-04 14:03:51 +0900

    Creating a Command Line Tool 6

--- docs/books/introduction-to-nodejs/building-cli-tools/cli-tools/bin/cmd.js
+++ docs/books/introduction-to-nodejs/building-cli-tools/cli-tools/bin/cmd.js
@@ -16,7 +16,7 @@ import got from "got";
 const API = "http://localhost:3000";
 
 /**
- *
+ * Log the usage of the command to the console
  * @param {*} msg
  */
 
@@ -34,14 +34,17 @@ const usage = (msg = "Back office for My App") => {
  * The second element in the `process.argv` array contains the `full path` to the file that has been executed with node (in this case the cmd.js file).
  * Neither of these are important to us,
  * so we use `process.argv.slice(2)` to create a new array (argv) that has the first two elements of the `process.argv` array chopped off.
+ *
  */
 const argv = process.argv.slice(2);
 
 /**
  * If there are no arguments, show the usage and exit
+ * 
  * So if there are less than two elements in the argv array, that is an input error.
  * We call the `usage` function with no arguments (which means it outputs the default msg in the usage function),
  * and then exit with a code of 1 (any non-zero code is a non-successful exit).
+ * 
  */
 //
 if (argv.length < 2) {
@@ -65,6 +68,7 @@ const [argID, argAmount] = argv;
  * Since all arguments that come from the command line are strings,
  * we convert the `amt` string to a number and assign it to the amount constant.
  * Amounts must be whole numbers, which we verify using Number.isInteger.
+ * 
  */
 //
 const amount = parseInt(argAmount); // Convert the `amt` string to a number and assign it to the amount constantl
@@ -75,6 +79,7 @@ const amount = parseInt(argAmount); // Convert the `amt` string to a number and 
  *
  * This will also return false if a non-numerical string has been passed to Number (it will be `NaN`, which is not an integer).
  *  If amount is not a valid integer we call the usage function — this time with a custom error message, and exit the process with a code of 1.
+ * 
  */
 if (isNaN(amount)) {
   usage("Error: <AMOUNT> must be a number");
@@ -82,17 +87,38 @@ if (isNaN(amount)) {
 }
 
 /**
- * Now for the actual meat of what we want our CLI to do: make a POST request to the `/orders/{ID}` route of our mock service.
+ * 
  */
 
-// Update the order with the given ID
+/**
+ * Update the order with the given ID
+ * 
+ * Now for the actual meat of what we want our CLI to do: 
+ * make a POST request to the `/orders/{ID}` route of our mock service.
+ * 
+ * The ESM format supports the Top-Level Await syntax. "type": "module",
+ * So the await keyword can be used outside any functions when using the ESM format
+ * 
+ */
+// 
 try {
   // Use `got` HTTP request library for Node.js to make a POST request to the API.
+  // Execution will pause until the promise return from got.post resolves or rejects.
   await got.post(`${API}/orders/${argID}`, { json: { amount } });
   // Log the result to the console
   console.log(`Order ${argID} updated with amount ${amount}`);
 } catch (error) {
-  // If there is an error, log it to the console and exit
+  /**
+   * If there is an error, log it to the console and exit
+   *
+   * It rejects in the case of a request responding with a 4xx or 5xx status code,
+   * or in case of any other errors, such as a network error.
+   * An awaited promise in a try block will fall through to the catch block if the awaited promise is rejected.
+   * So in the event of any error making the request,
+   * the catch block will log out whatever the error message might be,
+   * and then exit the process with a code of 1 to indicate it was not successful.
+   */
+
   console.log(error.message);
   process.exit(1);
 }
