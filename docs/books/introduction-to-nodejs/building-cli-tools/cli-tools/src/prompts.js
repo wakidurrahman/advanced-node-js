8319e9d1e3a1093c6c8d071e1ad6ac865a858883
wakidurrahman <wakidur.rahman@4digit.jp>
2024-01-06 14:35:00 +0900

    Adding Interactivity 3

--- docs/books/introduction-to-nodejs/building-cli-tools/cli-tools/src/prompts.js
+++ docs/books/introduction-to-nodejs/building-cli-tools/cli-tools/src/prompts.js
@@ -1,12 +1,17 @@
 import Enquirer from "enquirer";
-import { categories, listCategoryItems } from "./utils.js";
+import {
+  addProduct,
+  categories,
+  listCategoryItems,
+  updateProduct,
+} from "./utils.js";
 // Import the Enquirer prompt types
 const { prompt } = Enquirer;
 
 /**
  * First create an array of question objects that follow a predefined shape.
- * The type of question can have a number of different properties, which we will define using “autocomplete” or “input”. 
- * Then each question is assigned its own unique name, descriptive message for the question and a list of choices, 
+ * The type of question can have a number of different properties, which we will define using “autocomplete” or “input”.
+ * Then each question is assigned its own unique name, descriptive message for the question and a list of choices,
  */
 const categoryQuestions = [
   {
@@ -14,6 +19,54 @@ const categoryQuestions = [
     name: "category",
     message: "Category",
     choices: categories,
+  },
+];
+
+const orderQuestions = [
+  ...categoryQuestions,
+  {
+    type: "input",
+    name: "id",
+    message: "ID",
+  },
+  {
+    type: "input",
+    name: "name",
+    message: "Name",
+  },
+  {
+    type: "input",
+    name: "amount",
+    message: "Amount",
+  },
+  {
+    type: "input",
+    name: "info",
+    message: "Info",
+  },
+];
+
+const updateQuestions = [
+  {
+    type: "input",
+    name: "id",
+    message: "ID",
+  },
+  {
+    type: "input",
+    name: "amount",
+    message: "Amount",
+  },
+];
+
+const commandsList = ["add", "update", "list", "list by Id's", "help", "exit"];
+
+const commandsQuestions = [
+  {
+    type: "autocomplete",
+    name: "command",
+    message: "Command",
+    choices: commandsList,
   },
 ];
 
@@ -26,6 +79,17 @@ export const promptListIds = async () => {
   return listCategoryItems(category);
 };
 
-await promptListIds();
+export const promptAddOrder = async () => {
+  const { category, id, name, amount, info } = await prompt(orderQuestions);
+  return addProduct(category, id, name, amount, info);
+};
 
+export const promptUpdate = async () => {
+  const { id, amount } = await prompt(updateQuestions);
+  return updateProduct(id, amount);
+};
 
+export const promptCommand = async () => {
+  const { command } = await prompt(commandsQuestions);
+  return command;
+};
