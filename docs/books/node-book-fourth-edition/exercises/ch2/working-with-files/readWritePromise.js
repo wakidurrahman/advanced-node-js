dc8ef7d101253cbd667ade6d81172b52dfcb4618
wakidurrahman <wakidur.rahman@4digit.jp>
2024-01-12 19:52:05 +0900

    learned how we can interact with files using the fs Promises API

---
+++ docs/books/node-book-fourth-edition/exercises/ch2/working-with-files/readWritePromise.js
@@ -0,0 +1,27 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+const filepath = path.join(process.cwd(), "promise.txt");
+
+fs.readFile(filepath, "utf8")
+  .then((contents) => {
+    console.log("File Contents: ", contents);
+    const upperContents = contents.toUpperCase();
+
+    // Split the callbacks into named functions.
+    //   updateFile(filepath, upperContents);
+    updateFile(filepath, upperContents);
+  })
+  .catch((err) => {
+    if (err) return console.error("Error message ", err.message);
+  });
+
+function updateFile(filepath, contents) {
+  fs.writeFile(filepath, contents)
+    .then(() => {
+      console.log("File updated.");
+    })
+    .catch((err) => {
+      if (err) throw err;
+    });
+}
