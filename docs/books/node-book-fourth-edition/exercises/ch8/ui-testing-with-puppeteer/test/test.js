d19834e2578ceb9e0371403fd3737ffbdb20ac20
wakidurrahman <wakidur.rahman@4digit.jp>
2024-01-30 12:59:36 +0900

    when you run your tests, you will see the Chromium UI and can follow

--- docs/books/node-book-fourth-edition/exercises/ch8/ui-testing-with-puppeteer/test/test.js
+++ docs/books/node-book-fourth-edition/exercises/ch8/ui-testing-with-puppeteer/test/test.js
@@ -8,14 +8,34 @@
 const assert = require("assert");
 const puppeteer = require("puppeteer");
 
-const webURL = "https://example.com";
+const webDomainURL = "https://example.com";
 
 // We'll create an asynchronous function named runTest(), which will hold all of our test logic:
-async function runTest(params) {
+async function runTest(domain) {
   //need to launch Puppeteer
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: true });
   // need to create a new Puppeteer browser page
   const page = await browser.newPage();
   // instruct Puppeteer to load a URL. We do this by calling the goto() function on the page object
-  await page.goto(webURL);
+  await page.goto(domain);
+
+  // we can extract values from the web page by calling Puppeteer's $eval() function
+  const title = await page.$eval("h1", (el) => el.innerHTML);
+  const paragraph = await page.$eval("p", (el) => el.innerHTML);
+  console.log("Title value: ", title);
+  console.log("paragraph value: ", paragraph);
+  assert.equal(title, "Example Domain");
+  assert.equal(
+    paragraph,
+    "This domain is for use in illustrative examples in documents. You may use this\n" +
+      "    domain in literature without prior coordination or asking for permission."
+  );
+
+  /**
+   * call browser.close(),
+   * otherwise Puppeteer will continue emulating and the Node.js process will never exit.
+   */
+  browser.close();
 }
+
+runTest(webDomainURL);
