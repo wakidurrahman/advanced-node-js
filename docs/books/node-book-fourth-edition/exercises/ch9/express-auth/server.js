a4418763370b061f8077d9d625b23fc6f3a69fc7
wakidurrahman <wakidur.rahman@4digit.jp>
2024-01-31 13:09:35 +0900

    login, logout , post route created

--- docs/books/node-book-fourth-edition/exercises/ch9/express-auth/server.js
+++ docs/books/node-book-fourth-edition/exercises/ch9/express-auth/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const session = require("express-session");
 const { join } = require("path");
 
 const router = require("./routes/index.js");
+const auth = require("./routes/auth.js");
 const HOSTNAME = process.env.HOSTNAME || "0.0.0.0";
 const PORT = process.env.PORT || 3000;
 
@@ -11,12 +13,24 @@ const app = express();
 app.set("views", join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+// register the express-session middleware.
+app.use(
+  session({
+    name: "SESSIONID",
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: true },
+  })
+);
+
 app.use(
   bodyParser.urlencoded({
     extended: false, // option instructs body-parser to use the querystring library for URL parsing.
   })
 );
 
+app.use("/auth", auth);
 app.use("/", router);
 
 app.listen(PORT, HOSTNAME, () => {
