90028e62802ec510afe54cc12bc655fbfbd3bc0e
wakidurrahman <wakidur.rahman@4digit.jp>
2024-02-06 18:52:38 +0900

    Note that our log message has my-server prepended to it.

--- docs/books/node-book-fourth-edition/exercises/ch-12/express-debug-app/server.js
+++ docs/books/node-book-fourth-edition/exercises/ch-12/express-debug-app/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const debug = require("debug")("my-server");
 const app = express();
 
-
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => {
+  debug("HTTP GET request to /");
+  res.send("Hello World!");
+});
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
