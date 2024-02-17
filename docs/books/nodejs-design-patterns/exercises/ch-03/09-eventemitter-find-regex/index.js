88a7a7d388647d6e8afb7cf1c4a9a1e5f840fab8
wakidurrahman <wakidur.rahman@4digit.jp>
2024-02-17 12:04:53 +0900

    In the code we just defined

--- docs/books/nodejs-design-patterns/exercises/ch-03/09-eventemitter-find-regex/index.js
+++ docs/books/nodejs-design-patterns/exercises/ch-03/09-eventemitter-find-regex/index.js
@@ -2,6 +2,7 @@ import { EventEmitter, on } from 'events';
 import { readFile } from 'fs';
 
 const regExp = /hello \w+/g;
+const fileList = ['fileA.txt', 'fileB.json', 'fileC.json'];
 
 function findRegex(files, regex) {
   const emitter = new EventEmitter();
@@ -11,20 +12,20 @@ function findRegex(files, regex) {
 
     readFile(file, 'utf8', (err, content) => {
       if (err) {
-        return emitter.emit('error', err);
+        return emitter.emit('error', err); // ① error, when an error occurs during reading the file
       }
 
-      emitter.emit('fileread', file);
+      emitter.emit('fileread', file); // ② 'fileread', when a file is being read
       const match = content.match(regex);
       if (match) {
-        match.forEach((elem) => emitter.emit('found', file, elem));
+        match.forEach((elem) => emitter.emit('found', file, elem)); // ③ 'found', when a match has been found
       }
     });
   }
   return emitter;
 }
 
-findRegex(['fileA.txt', 'fileB.json'], regExp)
+findRegex(fileList, regExp)
   .on('fileread', (file) => console.log(`${file} was read`))
   .on('found', (file, match) => console.log(`Matched "${match}" in ${file}`))
   .on('error', (err) => console.log(`Error emitter ${err.match}`));
